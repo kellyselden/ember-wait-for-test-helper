16df1e2640a71b50168be697760cd91d2856ed98
Ryan T <ryanto@gmail.com>
2016-12-08 21:00:23 -0700

    Fix typo in variable name

--- addon/wait-for.js
+++ addon/wait-for.js
@@ -8,15 +8,15 @@ const {
   RSVP: { Promise }
 } = Ember;
 
-function _waitFor(app, selectorOfFn, interval = 1) {
+function _waitFor(app, selectorOrFn, interval = 1) {
   let waitForFn;
 
   // Support old API where you can pass in a selector as
   // a string and we'll wait for that to exist.
-  if (typeof selectorOfFn === "string") {
-    waitForFn = selectorToExist(selectorOfFn);
+  if (typeof selectorOrFn === "string") {
+    waitForFn = selectorToExist(selectorOrFn);
   } else {
-    waitForFn = selectorOfFn;
+    waitForFn = selectorOrFn;
   }
 
   return new Promise(resolve => {
