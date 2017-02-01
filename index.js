71b0ca6f6ad580c92270932ba07b631f27ba83c5
kellyselden <kellyselden@gmail.com>
2017-02-01 12:17:21 -0800

    disable in production

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-wait-for-test-helper'
+  name: 'ember-wait-for-test-helper',
+
+  included: function(app) {
+    this.app = app;
+
+    return this._super.included.apply(this, arguments);
+  },
+
+  treeFor: function() {
+    if (this.app.env === 'production') {
+      return;
+    }
+
+    return this._super.treeFor.apply(this, arguments);
+  }
 };
